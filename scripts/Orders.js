c488c1fc19b712b5bb07ecab72c9dc9a61345720
ts <shanksleslietrey@gmail.com>
2021-07-30 09:54:40 -0500

    initial commit

---
+++ scripts/Orders.js
@@ -0,0 +1,52 @@
+import { getOrders, addCustomOrder } from "./database.js"
+import { getMetals } from "./database.js"
+
+document.addEventListener(
+    "change",
+    (event) => {
+        if (event.target.id === "orderButton") {
+            addCustomOrder()
+        }
+    }
+)
+
+
+
+const buildOrderListItem = (order) => {
+    const metals = getMetals()
+
+// Remember that the function you pass to find() must return true/false
+const foundMetal = metals.find(
+    (metal) => {
+        return metal.id === order.metalId
+    }
+)
+const totalCost = foundMetal.price
+
+const costString = totalCost.toLocaleString("en-US", {
+    style: "currency",
+    currency: "USD"
+})
+
+    return `<li>
+        Order #${order.id} cost ${costString}
+    </li>`
+}
+
+export const Orders = () => {
+    /*
+        Can you explain why the state variable has to be inside
+        the component function for Orders, but not the others?
+    */
+    const orders = getOrders()
+
+    let html = "<ul>"
+
+    const listItems = orders.map(buildOrderListItem)
+
+    html += listItems.join("")
+    html += "</ul>"
+
+    return html
+}
+
